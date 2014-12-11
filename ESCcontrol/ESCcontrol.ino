925f911e73b219a84be062ad135580fdd454b47a
dark-spark <matt.syme@gmail.com>
2014-12-11 18:15:37 +1000

    Removed old code

--- ESCcontrol/ESCcontrol.ino
+++ ESCcontrol/ESCcontrol.ino
@@ -1,16 +1,12 @@
 #include <TimerOne.h>
 
-const int analogPin = 41;
 int val = 20;
 const int fanPin = 40;
 int pulseLength;
-boolean stringComplete = false;
-String inputString;
 int up = 12;
 int down = 13;
 boolean fetUp = false;
 boolean fetDown = false;
-int delayy;
 int repTime = 100;
 
 void setup() {
@@ -20,7 +16,6 @@ void setup() {
   pulseLength = 0;
   pinMode(up, INPUT_PULLUP);
   pinMode(down, INPUT_PULLUP);
-  delayy = millis();
 }
 
 void loop() {
@@ -36,36 +31,6 @@ void loop() {
     }
     pulseLength = 1000 + val;
   }
-
-
-void loop1() {
-
-  if(digitalRead(up) == LOW) {
-    if(fetUp == false) {
-      val ++;   
-      Serial.println(val);
-      fetUp = true;
-      delay(10);
-    }
-  }
-  if(digitalRead(up) == HIGH) {
-    fetUp = false;
-    delay(10);
-  }  
-  if(digitalRead(down) == LOW) {
-    if(fetDown == false) {
-      val --;   
-      Serial.println(val);
-      fetDown = true;
-      delay(10);
-    }
-  }
-  if(digitalRead(down) == HIGH) {
-    fetDown = false;
-    delay(10);
-  }    
-  pulseLength = 1000 + val;
-}
 
 void pulse() {
   digitalWrite(fanPin, HIGH);
